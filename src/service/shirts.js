1ad347c653b622088e2cb77e950d6980a4fbe956
dan <dan.bloodworth95@gmail.com>
2021-08-08 13:34:20 +0100

    Amend endpoints for shirts

--- src/service/shirts.js
+++ src/service/shirts.js
@@ -8,7 +8,7 @@ const axiosInstance = axios.create({
 export const GetShirts = async (dispatch) => {
     try {
         //api call to retrive the response.
-        const { data } = await axiosInstance.get();
+        const { data } = await axiosInstance.get('getallshirts');
 
         //Now that we have the response, we set the shirts state and pass it the response to
         //populate the shirts with...shirts :)
@@ -20,7 +20,7 @@ export const GetShirts = async (dispatch) => {
 
 export const DeleteShirt = async (dispatch, shirt) => {
     try {
-        await axiosInstance.delete(`/${shirt.id}`);
+        await axiosInstance.delete(`deleteshirt/${shirt.id}`);
         dispatch(ActionCreators.deleteShirt(shirt));
     } catch {
         console.log('Error deleting shirt!');
@@ -31,7 +31,7 @@ export const AddShirt = async (dispatch, shirt) => {
     try {
         //Shirt is currently a string so we make it into an object
         //for C# to parse properly.
-        const { data } = await axiosInstance.post('', shirt);
+        const { data } = await axiosInstance.post('addshirt', shirt);
         dispatch(ActionCreators.addShirt(data));
     } catch {
         console.log('Error adding shirt!');
@@ -40,7 +40,7 @@ export const AddShirt = async (dispatch, shirt) => {
 
 export const EditShirt = async (dispatch, shirt) => {
     try {
-        await axiosInstance.put('', shirt);
+        await axiosInstance.put('editshirt', shirt);
         dispatch(ActionCreators.editShirt(shirt));
     } catch {
         console.log('Error editing shirt!');
