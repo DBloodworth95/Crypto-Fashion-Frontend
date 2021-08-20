b0947a78cef90aa370489bac232962c39882dc6b
dan <dan.bloodworth95@gmail.com>
2021-08-20 22:55:22 +0100

    Amend admin panel route & add home page route

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { AdminSidebar } from './components/admin/AdminSidebar';
+import { HomeIndex } from './components/home/HomeIndex';
 
 const App = () => {
   return (
     <div className="App">
       <BrowserRouter>
         <Switch>
-          <Route path="/" exact component={AdminSidebar} />
+          <Route path="/admin" exact component={AdminSidebar} />
+          <Route path="/" exact component={HomeIndex} />
           <Route path="/" component={() => <div>Error 404: Page not found.</div>} />
         </Switch>
       </BrowserRouter>
