51ac0c37cb988e0e93c63641ae4ec65660e346cd
dan <dan.bloodworth95@gmail.com>
2021-08-21 11:41:52 +0100

    Add section

--- src/components/home/HomeIndex.js
+++ src/components/home/HomeIndex.js
@@ -7,22 +7,46 @@ import { makeStyles, Card, CardMedia, Typography } from "@material-ui/core";
 const useStyles = makeStyles(() => ({
     bannerCard: {
         position: "relative",
+        height: "600px",
+        marginBottom: "1rem"
     },
     cardMedia: {
-        height: 0,
-        paddingTop: "56.25%",
-
+        position: "relative",
+        height: "600px",
     },
     cardMediaOverlay: {
         position: "absolute",
-        top: "4%",
+        top: "10%",
         width: "100%",
         textAlign: "center",
         color: "white",
         backgroundColor: "none",
         fontFamily: "Open Sans, sans-serif",
         fontSize: "75px",
-    }
+    },
+    body: {
+        display: "flex",
+        flexDirection: "column",
+        height: "auto"
+    },
+    mainLeftContent: {
+        float: "left",
+        width: "30%",
+        padding: "20px",
+        //Remove this crap below later
+        height: "800px", 
+        borderStyle: "solid",
+        borderColor: "black",
+    },
+    mainRightContent: {
+        float: "left",
+        padding: "20px",
+        width: "70%",
+        //Remove this crap below later
+        height: "800px", 
+        borderStyle: "solid",
+        borderColor: "black",
+    },
 }));
 
 export const HomeIndex = () => {
@@ -34,24 +58,43 @@ export const HomeIndex = () => {
                 <Header />
             </header>
 
-            <section>
-                <Card className={classes.bannerCard}>
-                    <CardMedia  >
-                        <Image src="homebanner.jpg" imageStyle={{ height: "35%", width: "100%" }} />
-                    </CardMedia>
-                    <Typography gutterBottom variant="h1" component="h1" className={classes.cardMediaOverlay}>
-                        Welcome to CryptoFasion!
-                    </Typography>
-                </Card>
-            </section>
+            <body className={classes.body}>
 
-            <section>
-                <div>test</div>
-            </section>
+                <article className={classes.bannerCard}>
+                    <Card >
+                        <CardMedia className={classes.cardMedia} >
+                            <Image src="homebanner.jpg" imageStyle={{ height: "35%", width: "100%" }} />
+                        </CardMedia>
+                        <Typography gutterBottom variant="h1" component="h1" className={classes.cardMediaOverlay}>
+                            Welcome to CryptoFasion!
+                        </Typography>
+                    </Card>
+                </article>
+
+                <section>
+
+                <article className={classes.mainLeftContent}>
+                    <div>test</div>
+                    <div>test</div>
+                    <div>test</div>
+                    <div>test</div>
+                </article>
+
+                <article className={classes.mainRightContent}>
+                    <div>test</div>
+                    <div>test</div>
+                    <div>test</div>
+                    <div>test</div>
+                </article>
+
+                </section>
+
+            </body>
 
             <footer>
                 <Footer />
             </footer>
         </div>
+
     );
 }
