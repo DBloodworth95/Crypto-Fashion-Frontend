158f76524ec034c53b5fe98854a18ce95fac4242
IMulinge <31104209+IMulinge@users.noreply.github.com>
2021-09-08 14:09:27 +0100

    Login Directory fix

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { AdminSidebar } from './components/admin/AdminSidebar';
 import { HomeIndex } from './components/home/HomeIndex';
-import { Login } from './components/common/Login';
+import { Login } from './components/functions/Login';
 
 const App = () => {
   return (
@@ -13,7 +13,6 @@ const App = () => {
         <Switch>
           <Route path="/admin" exact component={AdminSidebar} />
           <Route path="/" exact component={HomeIndex} />
-          <Route path="/functions" exact component={Login} />
            <Route path="/Login" exact component={Login} />
           <Route path="/" component={() => <div>Error 404: Page not found.</div>} />
         </Switch>
--- src/components/functions/Login.js
+++ src/components/functions/Login.js
@@ -3,89 +3,114 @@ import { Footer } from "../common/Footer";
 import { Header } from "../common/Header";
 import Image from 'material-ui-image';
 import { makeStyles, Card, CardMedia, Typography, Grid } from "@material-ui/core";
-import { FeaturedProductsRow } from "./FeaturedProductsRow";
 
-const useStyles = makeStyles(() => ({
-    bannerCard: {
-        position: "relative",
-        height: "800px",
-        marginBottom: "1rem"
+
+
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Container from '@material-ui/core/Container';
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
     },
-    cardMedia: {
-        position: "relative",
-        height: "800px",
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
     },
-    cardMediaOverlay: {
-        position: "absolute",
-        top: "12%",
-        width: "100%",
-        textAlign: "center",
-        color: "white",
-        backgroundColor: "none",
-        fontFamily: "Roboto",
-        fontSize: "75px",
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(1),
     },
-    mainContent: {
-        padding: "20px",
-        width: "100%",
+    submit: {
+      margin: theme.spacing(3, 0, 2),
     },
-    mainContentHeader: {
-        textAlign: "center",
-        fontSize: "75px",
-        fontFamily: "Roboto",
-    },
-    mainContentCardContainer: {
-        flexGrow: 1,
-        textAlign: "center",
-        width: "100%",
-        padding: '100px'
-    }
-}));
+  }));
 
 export const Login = () => {
     const classes = useStyles();
 
     return (
         <div>
-            <header>
-                <Header />
-            </header>
-            <article className={classes.bannerCard}>
-                <Card >
-                    <CardMedia className={classes.cardMedia} >
-                        <Image src="homebanner.jpg" imageStyle={{ height: "55%", width: "100%" }} />
-                    </CardMedia>
-                    <Typography gutterBottom variant="h1" component="h1" className={classes.cardMediaOverlay}>
-                        Welcome to CryptoFasion!
-                    </Typography>
-                </Card>
-            </article>
-
-            <section>
-                <article className={classes.mainContent}>
-                    <Typography gutterBottom variant="h1" component="h1" className={classes.mainContentHeader}>
-                        Featured Products
-                    </Typography>
-                    <div className={classes.mainContentCardContainer}
-                    >
-                        <Grid container spacing={5}>
-                            <Grid container item xs={12} spacing={3}>
-                                <FeaturedProductsRow />
-                            </Grid>
-                            <Grid container item xs={12} spacing={3}>
-                                <FeaturedProductsRow />
-                            </Grid>
-                            <Grid container item xs={12} spacing={3}>
-                                <FeaturedProductsRow />
-                            </Grid>
-                        </Grid>
-                    </div>
-                </article>
-            </section>
-            <footer>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+         
+          <div className={classes.paper}>
+          <Typography component="h1" variant="h4">
+            CryptoFashion
+            </Typography>
+            <Avatar className={classes.avatar}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <form className={classes.form} noValidate>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+            </form>
+          </div>
+          <Box mt={8}>
+          </Box>
+        </Container>
+        <footer>
                 <Footer />
             </footer>
         </div>
-
-    );
+      );
 }
